3ce08665f8ace6bd1abb1dd3e27550f1de92fe06
Gary Tsang <gary@seriousbusiness.com>
2009-04-13 16:08:38 -0700

    Adding Scribe Server for db processing.

--- lib/ruby_scribe_client/scribe_server.rb
+++ lib/ruby_scribe_client/scribe_server.rb
@@ -1,19 +1,29 @@
 require File.join([File.dirname(__FILE__), 'scribe'])
-
-class ScribeHandler
-  def Log(messages)
-    messages.each do |x|
-      puts "#{x.inspect}"
-    end
-    return ResultCode::OK
+# 
+# class ScribeHandler
+#   def Log(messages)
+#     messages.each do |x|
+#       puts "#{x.inspect}"
+#     end
+#     return ResultCode::OK
+#   end
+# end
+# 
+# handler = ScribeHandler.new
+# 
+# puts "Starting the server..."
+# server.serve()
+# puts "done."
+class ScribeServer
+  def initialize(port, handler)
+    handler
+    processor = Scribe::Processor.new(handler)
+    transport = Thrift::ServerSocket.new(port)
+    transportFactory = Thrift::FramedTransportFactory.new
+    @server = Thrift::SimpleServer.new(processor, transport, transportFactory)  
+  end
+  
+  def run
+    @server.serve()
   end
 end
-
-handler = ScribeHandler.new
-processor = Scribe::Processor.new(handler)
-transport = Thrift::ServerSocket.new(1444)
-transportFactory = Thrift::FramedTransportFactory.new
-server = Thrift::SimpleServer.new(processor, transport, transportFactory)
-puts "Starting the server..."
-server.serve()
-puts "done."
