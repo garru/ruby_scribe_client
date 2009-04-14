27bbb5d326e9e22b7aed89f007db627d5fc11db5
Gary Tsang <gary@seriousbusiness.com>
2009-04-13 18:30:20 -0700

    nonblocking scribe server

--- lib/ruby_scribe_client/scribe_server.rb
+++ lib/ruby_scribe_client/scribe_server.rb
@@ -1,4 +1,5 @@
 require File.join([File.dirname(__FILE__), 'scribe'])
+require File.join([File.dirname(__FILE__), '..', '..', 'vendor','thrift','server','nonblockingserver'])
 # 
 # class ScribeHandler
 #   def Log(messages)
@@ -20,10 +21,22 @@ class ScribeServer
     processor = Scribe::Processor.new(handler)
     transport = Thrift::ServerSocket.new(port)
     transportFactory = Thrift::FramedTransportFactory.new
-    @server = Thrift::SimpleServer.new(processor, transport, transportFactory)  
+    @server = Thrift::NonblockingServer.new(processor, transport, transportFactory)  
   end
   
   def run
     @server.serve()
   end
 end
+
+# class ScribeHandler
+#   def Log(messages)
+#     messages.each do |x|
+#       puts "#{x.inspect}"
+#     end
+#     return ResultCode::OK
+#   end
+# end  
+# 
+# a = ScribeServer.new(1501, ScribeHandler.new)
+# a.run
